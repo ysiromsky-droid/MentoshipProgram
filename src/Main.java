16816845910b71a519dbc0ecbad0035beaf8985b
ysiromskyi <y.siromsky@gmail.com>
2026-01-05 16:43:46 +0200

    Variables check

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        /*
+        long longValue = 123456123l;
+        int intValue = 12345;
+        byte byteValue = 1;
+        short shortValue = 123;
+        double doubleValue = 1234.123456;
+        float floatValue = 123.456f;
+        char charValue = 'a';
+        String stringValue = "TEST Test test";
+        boolean boolValue = false;
+
+        System.out.println(longValue + " " + intValue + " " + byteValue + " " + shortValue + " " + doubleValue + " " + floatValue + " " + charValue + " " + stringValue + " " + boolValue);
+
+        String[] stringArray = {"Volvo", "BMW", "Ford", "Mazda"};
+        int[] intArray = {1, 3, 5, 8};
+        boolean[] boolArray = {false, true, true, false};
+
+        System.out.println(stringArray + " " + intArray + " " + boolArray);
+        */
+
+
+        double[] doubleArray = {0.5, 0.7, 1.0, 0.1};
+        float[] floatArray = {0.5f, 0.7f, 1.0f, 0.1f};
+        System.out.println(doubleArray);
+        System.out.println(floatArray);
+
     }
 }
